30b9b38f6a3aeb8f9d0013885ba75b5ca72dae55
Greg Ungerer <gerg@uclinux.org>
2016-09-23 13:44:29 +1000

    ARM: versatile: define empty debug_ll_io_init() for no-MMU
    No-MMU configured targets have no definition for debug_ll_io_init().
    Not all machines use this and it will only be required if CONFIG_DEBUG_LL
    is enabled.
    
    But when compiling for a target that uses it and it is configured for
    no-MMU (!CONFIG_MMU), for example the versatile machine, you will get:
    
      CC      arch/arm/mach-versatile/versatile_dt.o
    arch/arm/mach-versatile/versatile_dt.c: In function ‘versatile_map_io’:
    arch/arm/mach-versatile/versatile_dt.c:283:2: error: implicit declaration of function ‘debug_ll_io_init’ [-Werror=implicit-function-declaration]
      debug_ll_io_init();
      ^
    
    Fix by adding a macro for it to the !CONFIG_MMU path in map.h.
    
    Signed-off-by: Greg Ungerer <gerg@kernel.org>
    Reviewed-by: Vladimir Murzin <vladimir.murzin@arm.com>

--- arch/arm/include/asm/mach/map.h
+++ arch/arm/include/asm/mach/map.h
@@ -60,6 +60,7 @@ extern int ioremap_page(unsigned long virt, unsigned long phys,
 #else
 #define iotable_init(map,num)	do { } while (0)
 #define vm_reserve_area_early(a,s,c)	do { } while (0)
+#define debug_ll_io_init()	do { } while (0)
 #endif
 
 #endif
